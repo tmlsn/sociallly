77f0fa941ab0289cbe9b777ea070f9a57a89c6aa
elmehdi rhindi <g6handi@gmail.com>
2022-03-19 09:36:31 +0100

    db

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
 import { Tweet, ListOfTweets } from "./components";
 
+const tweet_db = [
+  { id: "1", content: "content1" },
+  { id: "2", content: "content2" },
+  { id: "3", content: "content3" },
+];
+
 function App() {
   return (
     <div>
