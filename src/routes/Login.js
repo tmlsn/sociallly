29b933900be91f4884f6c08cf7dd767540324f4a
elmehdi rhindi <g6handi@gmail.com>
2022-03-26 13:19:16 +0100

    login form setup

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,3 +1,19 @@
+import axios from "axios";
+import { useState } from "react";
+
 export function Login() {
-  return "login";
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form className="loginForm" onSubmit={handleSubmit}>
+      <input value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Login</button>
+    </form>
+  );
 }
